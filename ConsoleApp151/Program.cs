fab88d5c65d6aec9aeae1937b29b3d3424e1c8a1
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-12-31 22:22:37 +0330

    Add files via upload

---
+++ ConsoleApp151/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp151
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "started", "starting", "ended", "ending" };
+
+            foreach (var element in strings)
+            {
+                if (element.StartsWith("st"))
+                {
+                    Console.WriteLine($" \"{element}\" starts with \"st\"");
+                }
+            }
+
+            Console.WriteLine();
+
+
+
+            foreach (var element in strings)
+            {
+                if (element.EndsWith("ed"))
+                {
+                    Console.WriteLine($" \"{element}\" ends with \"ed\"");
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
